f35925576f0a5bc418f752b4da5144987e4c1877
rrose <rrose@gkl.home>
2015-11-05 11:23:02 -0800

    Use common sine() function for unit test

--- biquad_test.cpp
+++ biquad_test.cpp
@@ -5,16 +5,7 @@
 
 #include "EffectBiquad.h"
 
-void sine(float **audio_out, float freq, unsigned long samples, int sample_rate)
-{
-    float w = 2.0f * M_PI * freq / ((float) sample_rate);
-
-    for (unsigned long i = 0; i < samples; i++)
-    {
-        audio_out[0][i] = sin(((float) i) * w);
-        audio_out[1][i] = sin(((float) i) * w);
-    }
-}
+#include "sine.h"
 
 int main(int argc, char **argv)
 {
